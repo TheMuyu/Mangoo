332e62f6f275d90abb39cd9e05805abcdaf3381a
TheMuyu <themuyu@gmail.com>
2023-06-05 22:52:59 +0300

    Added Coupon Db

---
+++ Services/Mangoo.Services.CouponApi/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+using Mangoo.Services.CouponApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mangoo.Services.CouponApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
---
+++ Services/Mangoo.Services.CouponApi/Models/Coupon.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Mangoo.Services.CouponApi.Models
+{
+    public class Coupon
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public required string CouponCode { get; set; }
+        [Required]
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
